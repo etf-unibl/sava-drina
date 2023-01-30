034a8a0ab6defff1a565ff398629deebf37ede83
jovanabozic <116508532+jovanabozic@users.noreply.github.com>
2023-01-30 12:04:20 +0100

    Update D_FF.vhdl

--- hardware/design/D_FF/D_FF.vhdl
+++ hardware/design/D_FF/D_FF.vhdl
@@ -40,17 +40,19 @@ use ieee.STD_LOGIC_1164.all;
 
 entity D_FF is
   port( 
-       clk_i, reset_i,D_i: in std_logic;
-       Q_o: out std_logic);
+       clk_i   : in std_logic;
+       reset_i : in std_logic;
+       D_i     : in std_logic;
+       Q_o     : out std_logic);
 end D_FF;
 architecture beh of D_FF is
 begin
   process(clk_i,reset_i)
   begin
-    if(reset_i='1')then
-       Q_o <='0';
-    elsif(rising_edge(clk_i))then
-       Q_o <= D_i;
+    if(reset_i ='1') then
+      Q_o <= '0';
+    elsif(rising_edge(clk_i)) then
+      Q_o <= D_i;
     end if;
   end process;
 end beh;
